773ee24b4268614b17452d5b7e71f312b4dba135
mattcheang <cheangmatt@gmail.com>
2016-12-15 15:26:35 -0800

    change welcome.html.erb

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -3,24 +3,25 @@ class API::EventsController < ApplicationController
 
   before_filter :set_access_control_headers
 
- def set_access_control_headers
-   headers['Access-Control-Allow-Origin'] = '*'
-   headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
-   headers['Access-Control-Allow-Headers'] = 'Content-Type'
- end
+  def set_access_control_headers
+    headers['Access-Control-Allow-Origin'] = '*'
+    headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+    headers['Access-Control-Allow-Headers'] = 'Content-Type'
+  end
 
   def create
-     registered_application = App.find_by(url: request.env['HTTP_ORIGIN'])
-     event = registered_application.events.new(name: params[:name])
-    if event.save
-        render json: event, status: :created
+    registered_application = App.find_by(url: request.env['HTTP_ORIGIN'])
+    @event = registered_application.events.new(name: params[:name])
+
+    if @event.save
+      render json: @event, status: :created
     else
-        render json: {errors: event.errors}, status: :unprocessable_entity
+      render json: {errors: @event.errors}, status: :unprocessable_entity
     end
   end
 
   def preflight
-   head 200
+    head 200
   end
 
   private
