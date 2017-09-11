962b30e660d3e19ef10e3dc10ea00888a1b896b7
mattcheang <cheangmatt@gmail.com>
2017-09-11 16:02:23 -0700

    migrate database

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,5 +76,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-   config.action_mailer.default_url_options = { host: 'https://peaceful-wave-58477.herokuapp.com/' }
+   config.action_mailer.default_url_options = { host: 'https://metriks-api.herokuapp.com' }
 end
